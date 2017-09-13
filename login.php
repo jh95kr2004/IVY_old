e1bfa06515203be068f35cae34420fdfd05ba8c7
Jung Hoon Jang <junghoonjang@Jungui-MacBook-Pro.local>
2017-09-13 13:17:12 +0900

    Add login page (not completed yet)

---
+++ login.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang='en'>
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>
+
+  <title>IVY: Sign In</title>
+
+  <!-- ### Libraries ###
+  - jQuery 3.2.1
+  - Bootstrap 4.0.0-alpha.6 -->
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Javascript and CSS for this site -->
+  <script src="doc.js"></script>
+  <script src="js/consult.js"></script>
+  <link rel="stylesheet" href="doc.css">
+  <link rel="stylesheet" href="css/consult.css">
+</head>
+<body class='open-sans'>
+  <nav class="navbar navbar-toggleable-md navbar-light">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="index.html">IVY</a>
+  </nav>
+  <div id='mainContainer' class='container'>
+  </div>
+</body>
+</html>
